543760239f006a8bcd3043ba72b9b9e47547f5ec
abou5 <abou@laaltentech.com>
2023-12-31 11:53:31 +0530

    depth estimator library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,11 @@ android {
 }
 
 dependencies {
-
+    fileTree("libs").forEach { file ->
+        if (file.name.endsWith(".aar")) {
+            implementation(files(file))
+        }
+    }
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
